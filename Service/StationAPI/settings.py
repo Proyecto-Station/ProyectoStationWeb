f9e3e3f901b404c0f0604d8e0bc6e67d366668e4
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-04-16 11:56:20 -0400

    Setting DataBase Django

--- Service/StationAPI/settings.py
+++ Service/StationAPI/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,15 @@ WSGI_APPLICATION = 'StationAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('ENGINE'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'NAME': os.getenv('NAME'),
+        'OPTIONS': {
+            'init_command': os.getenv('OPTIONS'),
+        },
     }
 }
 
