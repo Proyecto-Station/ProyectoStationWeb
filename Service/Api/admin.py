21a748346aa64be52dee48081ed579f29dbcbbc1
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-04-25 16:27:48 -0400

    Añadido Clase Company

--- Service/Api/admin.py
+++ Service/Api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Username
+from .models import Company, Username
 
 admin.site.register(Username)
+admin.site.register(Company)
--- Service/Api/models.py
+++ Service/Api/models.py
@@ -1,3 +1,5 @@
+import email
+from pyexpat import model
 from django.db import models
 
 """
@@ -7,21 +9,43 @@ class Username(models.Model):
   GROUPS = (
     ('U', 'User'),
     ('C', 'Client'),
-    ('A', 'Admin'),
+    ('S', 'Sub-Client'),
+    ('A', 'Administrator'),
+  )
+  
+  SESSIONS = (
+    ('0', 'Offline'),
+    ('1', 'Online'),
   )
   
   name = models.CharField(max_length = 250)
   last_name = models.CharField(max_length = 250)
-  rut = models.CharField(max_length = 12)
+  rut = models.CharField(max_length = 12, unique = True)
   email = models.EmailField(max_length = 250)
   password = models.CharField(max_length = 50)
   permission = models.CharField(max_length = 1, choices = GROUPS)
-  session = models.IntegerField(default = 0)
+  session = models.IntegerField(default = 0, choices = SESSIONS)
+
+"""
+  Clase Empresas
+"""
+class Company(models.Model):
+  SWITCH = {
+    ('0', 'Inactive'),
+    ('1', 'Active'),
+  }
+
+  name = models.CharField(max_length = 250)
+  rut = models.CharField(max_length = 12, unique = True)
+  email = models.EmailField(max_length = 250)
+  address = models.CharField(max_length = 255)
+  status = models.IntegerField(default = 0, choices = SWITCH)
 
 """
   Clase Buses
 """
 class Bus(models.Model):
+  
   pass
 
 """
