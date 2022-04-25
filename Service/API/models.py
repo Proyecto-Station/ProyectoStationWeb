381de640889365aa3291c5af2e3679aa540d5f18
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-04-25 12:23:26 -0400

    Añadido modelo clase Usuario

--- Service/API/models.py
+++ Service/API/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+"""
+  Clase Usuarios
+"""
+class Username(models.Model):
+  GROUPS = (
+    ('U', 'User'),
+    ('C', 'Client'),
+    ('A', 'Admin'),
+  )
+  
+  name = models.CharField(max_length = 250)
+  last_name = models.CharField(max_length = 250)
+  rut = models.CharField(max_length = 12)
+  email = models.EmailField(max_length = 250)
+  password = models.CharField(max_length = 50)
+  permission = models.CharField(max_length = 1, choices = GROUPS)
+  session = models.IntegerField(default = 0)
+
+"""
+  Clase Buses
+"""
+class Bus(models.Model):
+  pass
+
+"""
+  Clase Choferes
+"""
+class Drivers(models.Model):
+  pass
