2e5082ba3f2534847ac630a5d6b81bd2e4e47c55
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-04-29 13:58:52 -0400

    update

--- station/src/index.js
+++ station/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import AppBarView from './view/AppBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AppBarView />
     <h1>Hola Mundo!</h1>
   </React.StrictMode>
 );
