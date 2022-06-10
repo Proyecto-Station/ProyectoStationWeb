f902ae41166ca152df9e8ca0686e034eed82bbe7
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-06-10 19:46:21 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 
@@ -37,30 +36,3 @@ const App = () => {
 }
 
 export default App
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 3ab4fa53098411571c59f9c9f1f1f65547e85582
