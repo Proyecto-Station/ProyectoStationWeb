142b8f39a2de858c5b1cc563bf02d008a3402edc
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-06-12 22:56:55 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './Components/Home'
 import Schedule from './Components/Schedule'
+import Reports from './Components/Reports'
 
 import NavBarMenu from './Components/Utils/NavBarMenu'
 
@@ -15,6 +16,7 @@ function App() {
         <Routes basename='/'>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/schedule' element={<Schedule />} />
+          <Route exact path='/report' element={<Reports />} />
           <Route path='*' element={<h1>Page not Found</h1>} />
         </Routes>
       </div>
