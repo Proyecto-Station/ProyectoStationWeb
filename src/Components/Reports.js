c99b723bcd99e47021eb9da51e5db923c58b89e5
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-06-14 10:06:52 -0400

    Update Solved Reports

--- src/Components/Reports.js
+++ src/Components/Reports.js
@@ -3,14 +3,31 @@ import React, { useState, useEffect } from 'react'
 import { Button } from '@mui/material';
 
 import jsPDF from 'jspdf';
+import 'jspdf-autotable'
 
 const generatePDF = () => {
   var doc = new jsPDF('p', 'pt');
 
+  const headers = [["NAME", "PROFESSION"]]
+
+  var itemNew = [
+    { id: 'Case Number', name : '101111111' },
+    { id: 'Patient Name', name : 'UAT DR' },
+    { id: 'Hospital Name', name: 'Dr Abcd' }
+  ]
+
+  const data = itemNew.map(elt=> [elt.id, elt.name])
+
+  let content = {
+    head: headers,
+    body: data
+  }
+
   doc.text(20, 20, 'This is the first title.')
-  doc.addFont('helvetica', 'normal')
   doc.text(20, 60, 'This is the second title.')
   doc.text(20, 100, 'This is the thrid title.')
+
+  doc.autoTable(content);
 
   doc.save('demo.pdf')
 }
