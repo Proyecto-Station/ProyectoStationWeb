bd84657b1aff1f5400715c5ab80d5af848627ec3
FantaCovid-19 <hugoalejandromoya@gmail.com>
2022-06-14 00:40:43 -0400

    Update

--- src/Components/Reports.js
+++ src/Components/Reports.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react'
 
-import { PDFDownloadLink } from "react-pdf";
+import { Button } from '@mui/material';
+
+import jsPDF from 'jspdf';
+
+const generatePDF = () => {
+  var doc = new jsPDF('p', 'pt');
+
+  doc.text(20, 20, 'This is the first title.')
+  doc.addFont('helvetica', 'normal')
+  doc.text(20, 60, 'This is the second title.')
+  doc.text(20, 100, 'This is the thrid title.')
+
+  doc.save('demo.pdf')
+}
 
 function Reports() {
   return (
-    <></>
+    <>
+    <Button sx={{ mt: 10 }} onClick={generatePDF}>print</Button>
+    </>
   )
 }
 
