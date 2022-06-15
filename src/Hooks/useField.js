22534bd47ae4d335f477a9a319b9699a83297a03
FantaCovid-19 <contact@mrfantasma.com>
2022-06-15 19:46:34 -0400

    Added useField

---
+++ src/Hooks/useField.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export function useField({ type }) {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
